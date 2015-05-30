935056b22b7722462484dba4c736a5e03c113704
Anton Magsino <MinecrafterPH@users.noreply.github.com>
2015-05-30 12:27:28 +0800

    Added popups upon death.

--- HungerGames/src/HG/Main.php
+++ HungerGames/src/HG/Main.php
@@ -478,7 +478,76 @@ class Main extends PluginBase implements Listener
 			
 		}
 	}
-	
+	public function onPlayerDie(PlayerDeathEvent $event){
+	          $p = $event->getPlayer();
+  $causeId = $p->getLastDamageCause()->getCause();
+  switch($causeId){
+    case EntityDamageEvent::CAUSE_DROWNING:
+      $text = "You drowned!";
+      break;
+    case EntityDamageEvent::CAUSE_FALL:
+      $text = "You fell from a high place!";
+      break;
+    case EntityDamageEvent::CAUSE_LAVA:
+      $text = "You tried to swim in lava!";
+      break;
+    case EntityDamageEvent::CAUSE_FIRE:
+      $text = "You burned to death!";
+      break;
+    case EntityDamageEvent::CAUSE_FIRE_TICK:
+      $text = "You burned to death!";
+      break;
+    case EntityDamageEvent::CAUSE_SUICIDE:
+      $text = "You died!"
+      break;
+    case EntityDamageEvent::CAUSE_SUFFOCATION: 
+      $text = "You suffocated in a wall!"
+      break;
+    case EntityDamageEvent::CAUSE_CONTACT:
+	if($cause instanceof EntityDamageByBlockEvent){
+	        if($cause->getDamager()->getId() === Block::CACTUS){
+		       $text = "You got pricked to death!";
+		}
+	}
+	break;
+    case EntityDamageEvent::CAUSE_PROJECTILE:
+	if($cause instanceof EntityDamageByEntityEvent){
+	$e = $cause->getDamager();
+		if($e instanceof Living){
+			$text = "You were shot by $params[]!";
+			$params[] = $e->getName();
+			break;
+		        }else{
+			$params[] = "Unknown";
+		}
+	}
+	break;
+    case EntityDamageEvent::CAUSE_ENTITY_ATTACK:
+        if($cause instanceof EntityDamageByEntityEvent){
+                if($e instanceof Living){
+                        $text = "You were slain by $params[]!";
+                        $param[] = $e->getName();
+                        break;
+                        }else{
+                        $params[] = "Unknown";
+		}
+	}
+	break;
+    case EntityDamageEvent::CAUSE_BLOCK_EXPLOSION:
+    case EntityDamageEvent::CAUSE_ENTITY_EXPLOSION:
+		if($cause instanceof EntityDamageByEntityEvent){
+		$e = $cause->getDamager();
+			if($e instanceof Living){
+				$text = "You were blown up by $params[]!";
+				$params[] = $e->getName();
+			}
+		}else{
+		$text = "You blew up!";
+	}
+	break;	
+  }
+  if(isset($text)) $p->sendPopup($text);
+	}
 	public function sendToAll($msg){
 		foreach($this->players as $pl)
 		{
