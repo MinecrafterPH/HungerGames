4e2333ad0c763c090f6fbbd495f97cd7a841029a
Keith The Koder <keithkfng@users.noreply.github.com>
2015-12-13 18:04:41 +1100

    Update Main.php

--- HungerGames/src/HG/Main.php
+++ HungerGames/src/HG/Main.php
@@ -619,7 +619,7 @@ class Main extends PluginBase implements Listener
 			case 4:
 			case 5:
 				$this->sendToAll(TextFormat::YELLOW."Starting in ".$this->lastTime.".");
-				$this->sendPopup(TextFormat::YELLOW."Starting in ".$this->lastTime.".")
+				$this->sendPopup(TextFormat::YELLOW."Starting in ".$this->lastTime.".");
 				break;	
 			case 10:
 				$this->sendToAll(TextFormat::YELLOW."The match will start in 0:10.");
